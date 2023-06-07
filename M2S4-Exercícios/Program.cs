d683f8e0872c696922c01fd6a9ebd73ba199d998
Willian Piva <willian.piva@gmail.com>
2023-06-07 20:15:28 -0300

    Add project files.

---
+++ M2S4-Exercícios/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Pessoa pessoa1 = new Pessoa("Loló", 14, "Rua José n° 260");
+pessoa1.Saudacao();
+
+
+class Pessoa
+{
+    private string Nome { get; set; }
+    protected int Idade { get; set; }
+    public string Endereco { get; set; }
+
+    public Pessoa(string nome, int idade, string endereco)
+    {
+        this.Nome = nome;
+        this.Idade = idade;
+        this.Endereco = endereco;
+    }
+    public void Saudacao()
+    {
+        Console.WriteLine($"Olá, meu nome é {Nome} e eu tenho {Idade} anos");
+    }
+}
